c69ca13196d1d14e6b92eed1bc6215cc667a32d7
leandro-etec-2025 <leandrocesardacruz.cps.2025@gmail.com>
2025-03-20 12:30:44 -0300

    Adicionado apenas os prints.

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -2,7 +2,12 @@
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Imprimir mensagem
+		System.out.println("Hello, world!"); // sysout & CTRL + SPACE
+		
+		// Imprimir mensagem sem pular linha
+		System.out.printf("Olá, mundo!");
 
 	}
 
